9fa991a218424976a3c0f1a32457fc8dc5c4d0e5
Luiz Lopes <luizlopes12@outlook.com>
2021-05-22 21:08:42 -0300

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 function navOpen(){
-    console.log('Tudo funfando chefia');
+    let nav = document.querySelector('#nav');
+    nav.setAttribute('id', 'navbar-open');
+
+
+}
+function navClose(){
+    let nav = document.querySelector('#navbar-open');
+    nav.removeAttribute('id', 'nav');
 }
